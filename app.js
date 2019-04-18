0de53938999d2c018a263ca7c1bdcccd87b3497c
Arin Trongsantipong <arintrongs@gmail.com>
2019-04-18 14:42:53 +0700

    Add users & room endpoints

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var roomList = [];
+
+var rooms = {};
+
 app.get("/", (req, res) => {
   console.log("Hi");
 });
@@ -14,4 +17,55 @@ app.put("/allrooms", (req, res) => {
   console.log(roomList, roomList.indexOf(req.body.id));
 });
 
+// room
+app.get("/room/:id", (req, res) => {
+  const room_id = req.params.id;
+  return res.status(200).send(rooms[room_id].users);
+});
+app.post("/room", (req, res) => {
+  const room_id = req.params.id;
+  const username = req.body.user;
+  const idx = rooms[room_id].users.indexOf(username);
+  if (idx !== -1) {
+    return res.status(200).send({});
+  } else {
+    rooms[room_id].users.push(username);
+    return res.status(201).send({});
+  }
+});
+app.put("/room/:id", (req, res) => {
+  const room_id = req.params.id;
+  const username = req.body.user;
+  const idx = rooms[room_id].users.indexOf(username);
+  if (idx !== -1) {
+    return res.status(200).send({});
+  } else {
+    rooms[room_id].users.push(username);
+    return res.status(201).send({});
+  }
+});
+app.delete("/room/:id", (req, res) => {
+  const room_id = req.params.id;
+  const username = req.body.user;
+  const idx = rooms[room_id].users.indexOf(username);
+  if (idx !== -1) {
+    rooms[room_id].users = rooms[room_id].users.filter(
+      user => user !== username
+    );
+    return res.status(200).send(`${username} leaves the room`);
+  } else {
+    rooms[room_id].users.push(username);
+    return res.status(201).send(`${username} is not found`);
+  }
+});
+// user
+app.get("/users", (req, res) => {
+  const users = {};
+  for (let key of Object.keys(rooms)) {
+    for (let user of room[key].users) {
+      users[user] = "";
+    }
+  }
+  return res.status(200).send(Object.keys(users));
+});
 module.exports = app;
